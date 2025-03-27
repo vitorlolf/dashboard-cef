f3c4e2e289ad2ba30d99441583843eac9b72e4d9
Vitor Franco <vitor.franco@serges.org>
2025-03-27 14:42:33 -0300

    Corrige execução automática para compatibilidade com Render

--- dashboard_cef.py
+++ dashboard_cef.py
@@ -256,7 +256,7 @@ server = app.server
 import os
 
 if __name__ == "__main__":
-    print("Iniciando Dashboard...")
     port = int(os.environ.get("PORT", 8050))
     app.run(host="0.0.0.0", port=port)
+    
 
